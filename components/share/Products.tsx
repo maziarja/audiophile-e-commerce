85d1c0395f2d79e02b1bfb39f8a9a2637f870d2c
maziarja <maziar.ja@gmail.com>
2025-12-06 13:55:11 -0800

    test: trying to solve error in prod v3

--- components/share/Products.tsx
+++ components/share/Products.tsx
@@ -4,6 +4,7 @@ import earphone from "@/public/assets/earphones-no-bg.png";
 import speaker from "@/public/assets/speaker-no-bg.png";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
+import ProductLink from "./ProductLink";
 
 const products = [
   { image: headphone, title: "headphones" },
@@ -40,8 +41,8 @@ function Products({
               <p className="mt-22 text-center text-[15px] font-bold tracking-[1.07px] uppercase">
                 {product.title}
               </p>
-              <Link
-                // onClick={closeMenu}
+              {/* <Link
+                onClick={closeMenu}
                 href={`/category?type=${product.title}`}
                 className="mx-auto flex items-center gap-1 text-center"
               >
@@ -49,7 +50,8 @@ function Products({
                   SHOP
                 </span>
                 <ChevronRight color="#d87d4a" size={20} />
-              </Link>
+              </Link> */}
+              <ProductLink title={product.title} closeMenu={closeMenu} />
             </div>
           </div>
         ))}
