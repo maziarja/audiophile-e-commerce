0d38d628bccdbe193d30c05f78ed55ccd65b3063
maziarja <maziar.ja@gmail.com>
2025-12-06 15:58:10 -0800

    test: trying to solve error in prod v30

--- models/test.ts
+++ models/test.ts
@@ -1,14 +1,18 @@
-import mongoose, { Document, Model, models, Schema } from "mongoose";
+import mongoose, { Schema, models, Model, Document } from "mongoose";
 
-export type TestType = {
+export interface TestType extends Document {
   name: string;
-};
+}
 
-const testSchema = new Schema<TestType & Document>({
-  name: String,
-});
+const testSchema = new Schema<TestType>(
+  {
+    name: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
-const Test: Model<TestType & Document> =
-  models.Test || mongoose.model<TestType & Document>("Test", testSchema);
+// On Vercel serverless, you MUST do this check to avoid recompilation
+const Test: Model<TestType> =
+  models.Test || mongoose.model<TestType>("Test", testSchema);
 
 export default Test;
