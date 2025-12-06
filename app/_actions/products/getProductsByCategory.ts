4d4dc74b977f726e733108f48d6c3be54c465eb7
maziarja <maziar.ja@gmail.com>
2025-12-06 15:08:35 -0800

    test: trying to solve error in prod v19

--- app/_actions/products/getProductsByCategory.ts
+++ app/_actions/products/getProductsByCategory.ts
@@ -8,7 +8,7 @@ import Product from "@/models/Products";
 export async function getProductByCategory(category: string) {
   try {
     await connectDB();
-    const productsDoc = await Product.find({ price: 599 })
+    const productsDoc = await Product.find({ category })
       .sort({ _id: -1 })
       .select(["categoryImage", "description", "new", "name", "category"])
       .lean();
