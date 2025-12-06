dfd8061d35211a69aa4798e9ee86cc0e2677d1af
maziarja <maziar.ja@gmail.com>
2025-12-06 14:14:01 -0800

    test: trying to solve error in prod v9

--- app/_actions/products/getProductsByCategory.ts
+++ app/_actions/products/getProductsByCategory.ts
@@ -2,24 +2,28 @@
 
 import { convertToObject } from "@/lib/convertToObject";
 import connectDB from "@/lib/database";
-import { ProductsByCategorySchema } from "@/lib/schemas/productType";
+import {
+  ProductsByCategorySchema,
+  ProductType,
+} from "@/lib/schemas/productType";
 import Product from "@/models/Products";
 
 export async function getProductByCategory(category: string) {
   try {
     await connectDB();
     const productsDoc = await Product.find({ category })
-      .sort({ _id: -1 })
-      .select(["categoryImage", "description", "new", "name", "category"])
+      // .sort({ _id: -1 })
+      // .select(["categoryImage", "description", "new", "name", "category"])
       .lean();
     const products = convertToObject(productsDoc);
 
-    const result = ProductsByCategorySchema.safeParse(products);
-    if (!result.success) {
-      throw new Error("Error: " + result.error.issues[0].message);
-    }
+    // const result = ProductsByCategorySchema.safeParse(products);
+    // if (!result.success) {
+    //   throw new Error("Error: " + result.error.issues[0].message);
+    // }
 
-    if (result.success) return result.data;
+    // if (result.success) return result.data;
+    return products as ProductType[];
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch products");
