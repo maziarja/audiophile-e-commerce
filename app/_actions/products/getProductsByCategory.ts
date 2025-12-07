14ffa2cd01f6d7e336282d98188252263a87c2cf
maziarja <maziar.ja@gmail.com>
2025-12-07 12:42:51 -0800

    deleting mongodb clauster and creading new one on vercel

--- app/_actions/products/getProductsByCategory.ts
+++ app/_actions/products/getProductsByCategory.ts
@@ -13,13 +13,14 @@ export async function getProductByCategory(category: string) {
       .select(["categoryImage", "description", "new", "name", "category"])
       .lean();
     const products = convertToObject(productsDoc);
-
     const result = ProductsByCategorySchema.safeParse(products);
     if (!result.success) {
       throw new Error("Error: " + result.error.issues[0].message);
     }
 
     if (result.success) return result.data;
+
+    return productsDoc;
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch products");
